53fc70e44e06b2bf32e5c93edf756261265f2f66
Anna <s16782@pjwstk.edu.pl>
2020-03-06 14:36:25 +0100

    Dodanie i obsluga home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//koniec pica tej kawusi kurwa mac
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AlpakiComponent } from './alpaki/alpaki.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddAlpacaComponent } from './add-alpaca/add-alpaca.component';
+import {AlpacaService} from '../services/alpaca.service';
+import {OwnerService} from '../services/owner.service';
+import {SpeciesService} from '../services/species.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AlpakiComponent,
+    GalleryComponent,
+    AboutComponent,
+    ContactComponent,
+    AddAlpacaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AlpacaService, OwnerService, SpeciesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
